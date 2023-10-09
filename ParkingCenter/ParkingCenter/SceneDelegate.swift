0e4ebb6e9e57eb2d20e87674f1af87aba63a523e
MaryJo-github <jotjdal0215@gmail.com>
2023-10-09 15:37:54 +0900

    ✨ Feat: 프로젝트 생성 및 main storyboard 삭제

---
+++ ParkingCenter/ParkingCenter/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  ParkingCenter
+//
+//  Created by MARY on 2023/10/09.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let rootViewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ ParkingCenter/ParkingCenter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ParkingCenter
+//
+//  Created by MARY on 2023/10/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
